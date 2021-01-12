3495cff4a1b80fd645b5640ecaa5590731b1146b
Ian Philip Eglin <philip@eglin.no>
2021-01-12 15:34:46 +0100

    Added comments to JS1 assigment

--- Øvinger/js-ov01-ian-philip-eglin/index.js
+++ Øvinger/js-ov01-ian-philip-eglin/index.js
@@ -1,4 +1,5 @@
 
+
 // OPPGAVE 1
 
 // Creating a class and a constructor for cars containing properties (registration number, car brand, model and current speed)
@@ -19,14 +20,16 @@ let car1 = new Bil("41492", "Fiat", 2008, 40);
 let car2 = new Bil("911", "Porsche", 2019, 120);
 let car3 = new Bil("67482", "Audi", 2017, 90);
 
+
 // OPPGAVE 3
 
-
+// Creating methods that allows all of the cars to both accellerate and break
 Bil.prototype.gass = function () {
     return this.hastighet += 10;
 }
 
 Bil.prototype.brems = function () {
+    // Limiting the  function so that the cars cannot obtain a negative speed. Which would mean it is in reverse... Obviously
     if (this.hastighet > 10) {
         return this.hastighet -= 10;
     }
@@ -35,7 +38,10 @@ Bil.prototype.brems = function () {
     }
 }
 
+
 // OPPGAVE 4
+
+// Creating functions that will prompt the respective car to break or gass
 function gassCar(id) {
     switch (id) {
         case "gassBtn1":
@@ -70,11 +76,15 @@ function breakCar(id) {
     updateSpeedometer();
 }
 
+
 // OPPGAVE 5
+
+// Adding a method to the class that will return a string with relevant information of the respective car.
 Bil.prototype.status = function() {
     return this.merke + ": " + this.hastighet + " km/t\n";
 }
 
+// Updating the speedometer DIV to let the user know each cars' status.
 function updateSpeedometer() {
     document.getElementById("speedometer").innerText = car1.status() + car2.status() + car3.status();
 }
