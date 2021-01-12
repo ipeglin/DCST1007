303e9fbdc7dd00251d1cff52aa4db255e5ce6472
Ian Philip Eglin <philip@eglin.no>
2021-01-12 18:26:20 +0100

    Made most of the functions required by point 5). Some small adjustments remain

--- Assignments/js-ov02-ian-philip-eglin/index.js
+++ Assignments/js-ov02-ian-philip-eglin/index.js
@@ -22,9 +22,6 @@ class ChildrensAccount extends Account {
 let adult = new Account(194, "Stian Hansen", 0);
 let child = new ChildrensAccount(141, "Erlend Hansen");
 
-// console.log(adult);
-// console.log(child);
-
 
 // OPPGAVE 3
 Account.prototype.deposit = function (sum) {
@@ -48,11 +45,11 @@ Account.prototype.accountInformation = function () {
 
 let kari = new Account(93827100, "Kari Hansen", 429);
 
-// console.log(kari)
-
 
 // OPPGAVE 5
 let checkbox = document.getElementById("isChild");
+let customerNumbers = [];
+let customerTickers = [];
 
 checkbox.onchange = () => {
     if (checkbox.checked) {
@@ -64,11 +61,52 @@ checkbox.onchange = () => {
     }
 }
 
+function generateCustomerNr() {
+    let number = customerNumbers.length + 1;
+    customerNumbers.push(number);
+    return number;
+}
+
 function createAccount() {
-    let name = document.getElementById("name-input");
-    let deposit = document.getElementById("deposit-input")
+    let array = [];
+    let name = document.getElementById("name-input").value;
+    array.push(name);
+    let deposit = document.getElementById("deposit-input").value == "" ? 0 : parseInt(document.getElementById("deposit-input").value);
     let isChild = checkbox.checked == true ? true : false;
     if (isChild) {
-        
+        for (let i of array) {
+            this["" + name.split(" ")[0].toLowerCase()] = new ChildrensAccount(generateCustomerNr(), name);
+            customerTickers.push("" + name.split(" ")[0].toLowerCase());
+        }
     }
+    else {
+        for (let i of array) {
+            this["" + name.split(" ")[0].toLowerCase()] = new Account(generateCustomerNr(), name ,deposit);
+            customerTickers.push("" + name.split(" ")[0].toLowerCase());
+        }
+    }
+    array.shift();
+    console.log("Account created for: " + name + " under username: " + name.split(" ")[0].toLowerCase());
+}
+
+function transferAmount() {
+    let amount = parseFloat(document.getElementById("transfer-sum-input").value);
+    let sender = document.getElementById("sender").value;
+    let reciever = document.getElementById("reciever").value;
+    this[sender].withdraw(amount);
+    this[reciever].deposit(amount);
+}
+
+function showAccounts() {
+    for (let i of customerTickers) {
+        console.log(this[i]);
+    }
+}
+
+function depositAmount() {
+    this[document.getElementById("deposit-name-input").value.toLowerCase()].deposit(parseFloat(document.getElementById("deposit-amount").value));
+}
+
+function withdrawAmount() {
+    this[document.getElementById("withdraw-name-input").value.toLowerCase()].withdraw(parseFloat(document.getElementById("withdraw-amount").value));
 }
