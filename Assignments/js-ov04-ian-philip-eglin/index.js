acb638e9714edb1e2d934b7fedb68b1ddbbd1ee0
Ian Philip Eglin <philip@eglin.no>
2021-01-13 16:49:08 +0100

    Partially finished point 1) for assignment OOP4. Number validation works, but needs a quality check

---
+++ Assignments/js-ov04-ian-philip-eglin/index.js
@@ -0,0 +1,51 @@
+
+
+// OPPGAVE 1
+
+
+// Validate number
+const validateNumber = function (number) {
+    return new Promise(function (resolve, reject) {
+        if (!isNaN(number) && typeof (number) == "number") {
+            console.log(`${number} is a valid number`)
+            resolve(number);
+        }
+        else {
+            reject(new Error("Input was not a number"))
+        }
+    });
+}
+
+// Check number
+const valueNumber = function (number) {
+    return new Promise(function (resolve) {
+        let output;
+        if (number != 10) {
+            var keyword = number > 10 ? "større" : "mindre";
+            output = `${number} er ` + keyword + " enn verdien 10."
+        }
+        else {
+            output = `${number} er av samme verdi som 10.`;
+        }
+        resolve(output);
+    })
+}
+
+// Print number
+
+validateNumber(2).then(res => console.log(valueNumber(res))).catch(err => console.error(err));
+
+// SLUTT
+
+
+
+// OPPGAVE 2
+
+
+
+// SLUTT
+
+
+
+// OPPGAVE 3
+
