7819a8b205c358bb37b1c2c0e1025f136a98f310
ipeglin <philip@eglin.no>
2022-03-08 23:11:15 +0100

    Added jsdocs comments for service modules in assignment 8

--- Assignments/js-ov08-ian-philip-eglin/src/services.js
+++ Assignments/js-ov08-ian-philip-eglin/src/services.js
@@ -1,6 +1,15 @@
 import { pool } from './mysql-pool';
 
+/**
+ * Creates a service class API for students database
+ * @class
+ */
 class StudentService {
+  /**
+   * @method getStudents Get all students from database
+   * 
+   * @param {function} success Callback function for adding students
+   */
   getStudents(success) {
     pool.query('SELECT * FROM Students', (error, results) => {
       if (error) return console.error(error);
@@ -9,6 +18,12 @@ class StudentService {
     });
   }
 
+  /**
+   * @method getStudent Get a specific student from database
+   * 
+   * @param {int} id Student id in database
+   * @param {function} success Callback function for retrieving student
+   */
   getStudent(id, success) {
     pool.query('SELECT * FROM Students WHERE id=?', [id], (error, results) => {
       if (error) return console.error(error);
@@ -17,6 +32,11 @@ class StudentService {
     });
   }
 
+  /**
+   * @method addStudent Add a new student to database
+   * 
+   * @param {function} success Callback function 
+   */
   addStudent(success) {
     pool.query(
       'INSERT INTO Students (name, email, programID) VALUES (?, ?, ?)',
@@ -29,6 +49,12 @@ class StudentService {
     );
   }
 
+  /**
+   * @method removeStudent
+   * 
+   * @param {int} id  Student id in database
+   * @param {function} success Callback function 
+   */
   removeStudent(id, success) {
     pool.query('SELECT name FROM Students WHERE id=?', [id], (error, results) => {
       if (error) return console.error(error);
@@ -49,6 +75,14 @@ class StudentService {
     });
   }
 
+  /**
+   * @method addStudent Add a new student to database
+   * 
+   * @param {string} name Name of new student 
+   * @param {string} email Email of new student 
+   * @param {int} programID ID of the studyprogram the student is enrolled in
+   * @param {function} success Callback function
+   */
   addStudent(name, email, programID, success) {
     pool.query(
       'INSERT INTO Students (name, email, programID) VALUES (?,?,?)',
@@ -70,6 +104,12 @@ class StudentService {
     );
   }
 
+  /**
+   * @method updateStudent Update database values for a specific student
+   * 
+   * @param {string} student Name of the student 
+   * @param {function} success Callback function
+   */
   updateStudent(student, success) {
     pool.query(
       'UPDATE Students SET name=?, email=? WHERE id=?',
@@ -84,7 +124,16 @@ class StudentService {
 }
 export let studentService = new StudentService();
 
+/**
+ * Creates a service class API for study program database
+ * @class
+ */
 class ProgramService {
+  /**
+   * @method getPrograms Get all programs from database
+   * 
+   * @param {function} success Callback function 
+   */
   getPrograms(success) {
     pool.query('SELECT * FROM StudyPrograms', (error, results) => {
       if (error) return console.error(error);
@@ -93,6 +142,12 @@ class ProgramService {
     });
   }
 
+  /**
+   * @method getProgram Get specific study program from database
+   * 
+   * @param {int} id Study program ID
+   * @param {function} success Callback function
+   */
   getProgram(id, success) {
     pool.query('SELECT * FROM StudyPrograms WHERE id=?', [id], (error, results) => {
       if (error) return console.error(error);
@@ -101,6 +156,12 @@ class ProgramService {
     });
   }
 
+  /**
+   * @method getParticipants Get all students in program group from database
+   * 
+   * @param {int} groupID ID of the study program group
+   * @param {function} success  Callback function
+   */
   getParticipants(groupID, success) {
     let list;
     pool.query('SELECT * FROM Participants WHERE groupID=?', [groupID], (error, results) => {
@@ -116,6 +177,12 @@ class ProgramService {
     });
   }
 
+  /**
+   * @method removeProgram Removed a studyprogram from database
+   * 
+   * @param {int} id ID of the studyprogram
+   * @param {function} success  Callback function
+   */
   removeProgram(id, success) {
     pool.query('DELETE FROM StudyPrograms WHERE id=?', [id], (error, results) => {
       if (error) return console.error(error);
@@ -124,6 +191,12 @@ class ProgramService {
     });
   }
 
+  /**
+   * @method updateProgram database values for a specific study program
+   * 
+   * @param {string} program New name for the study program
+   * @param {function} success Callback function
+   */
   updateProgram(program, success) {
     pool.query(
       'UPDATE StudyPrograms SET programName=?, programCode=? WHERE id=?',
@@ -136,6 +209,14 @@ class ProgramService {
     );
   }
 
+  /**
+   * @method addProgram Add a new study program to database
+   * 
+   * @param {string} name Name of the new study program
+   * @param {string} code Subject code of the new study program
+   * @param {int} group Participant group ID
+   * @param {function} success  Callback function
+   */
   addProgram(name, code, group, success) {
     pool.query(
       'INSERT INTO StudyPrograms (programName, programCode, participantGroup) VALUES (?,?,?)',
