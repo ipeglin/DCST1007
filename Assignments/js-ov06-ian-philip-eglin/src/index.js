ae1b9ad5a7682454f99821112222c3512350123f
ipeglin <philip@eglin.no>
2022-03-08 22:16:20 +0100

    Added jsdocs comments for assignment 6

--- Assignments/js-ov06-ian-philip-eglin/src/index.js
+++ Assignments/js-ov06-ian-philip-eglin/src/index.js
@@ -3,7 +3,15 @@ import { Component } from 'react-simplified';
 import ReactDOM from 'react-dom';
 import { NavLink, HashRouter, Route } from 'react-router-dom';
 
+/**
+ * Menu component for navigation
+ */
 class Menu extends Component {
+  /**
+   * Renders the menu to the DOM
+   * 
+   * @returns {div} Div holding the nivbar
+   */
   render() {
     return (
       <div>
@@ -20,30 +28,47 @@ class Menu extends Component {
   }
 }
 
+/**
+ * Home page component
+ */
 class Home extends Component {
   render() {
     return <div><h1>*Your Name Here*</h1><h3><i>*Short Text About You*</i></h3></div>;
   }
 }
 
+/**
+ * Education page component
+ */
 class Education extends Component {
   render() {
     return <div>*Your Education Listed Here*</div>;
   }
 }
 
+/**
+ * Work Experience page component
+ */
 class WorkExperience extends Component {
   render() {
     return <div>*Your Previous Work Experience Here*</div>;
   }
 }
 
+/**
+ * Skills page component
+ */
 class Skills extends Component {
   render() {
     return <div>*Your Additional Skills Here*</div>
   }
 }
 
+/**
+ * Renderer for the React DOM holding a routing for page components
+ * NB! This method of routing is outdated. Check React documentation for the
+ *    new approach.
+ */
 ReactDOM.render(
   <HashRouter>
     <div>
